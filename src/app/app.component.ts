3381c871c174e2627e100cf90d41a88c8463333d
Dredheza Selimaj <selimaj.dredheza@gmail.com>
2022-04-11 22:18:19 +0200

    Implement Dev Platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dev-platform';
+
+  cards=[
+    {title: 'Google', imgPath:'google.png', href: 'https://www.google.com' },
+    {title: 'Linkedin', imgPath:'linkedin.png', href: 'https://www.linkedin.com' },
+    {title: 'Angular', imgPath:'angular.png', href: 'https://angular.io/' },
+    {title: 'FreeCodeCamp', imgPath:'freecodecamp.jpg', href: 'https://www.freecodecamp.org/' },   
+    {title: 'Dev', imgPath:'dev.png', href: 'https://dev.to/' },
+    {title: 'W3Schools', imgPath:'w3s.png', href: 'https://www.w3schools.com/'},
+    {title: 'Youtube', imgPath:'youtube.png', href: 'https://www.youtube.com/' },
+    {title: 'GitHub', imgPath:'github.png', href: 'https://github.com/' },
+    {title: 'Udemy', imgPath:'udemy.jpg', href: 'https://www.udemy.com/' },
+   
+  ];
 }
